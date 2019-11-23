49b8589a8c6e87a3ed9b522f34512be00a2ae5bb
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-23 23:20:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ attr_accessor :items, :total, :discount, :last_transaction
   end
 
   def add_item(title, price, quantity = 1)
-    quantity.times.do
+    quantity.times do
       @items << title
     end
   self.total += price*quantity
